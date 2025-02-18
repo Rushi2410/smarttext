ff8c84dedbb7f2455aa9a20be4ef01fd5d011ffc
Rushi2410 <rushikeshandhale1010@gmail.com>
2025-02-18 17:04:34 +0530

    Updates

--- src/components/DarkModeToggle.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function DarkModeToggle({ darkMode, toggleDarkMode }) {
-  return (
-    <button className="btn btn-dark mb-3" onClick={toggleDarkMode}>
-      {darkMode ? "Light Mode ☀️" : "Dark Mode 🌙"}
-    </button>
-  );
-}
